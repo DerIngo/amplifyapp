8dcb5304793ce8a42258083c8a1115f84b55eabc
DerIngo <DerIngo@users.noreply.github.com>
2020-10-08 19:28:16 +0200

    added build v2

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          We now have Auth!<br/>And we can build.
+          We now have Auth!<br/>And we hopefully really can build.
         </p>
       </header>
       <AmplifySignOut />
